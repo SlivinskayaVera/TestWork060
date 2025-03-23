900772d55b455a60a72f27bd4b390020dded922e
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-03-23 20:35:22 +0300

    remoting unused imports

--- src/app/components/ForecastList/ForecastList.tsx
+++ src/app/components/ForecastList/ForecastList.tsx
@@ -1,8 +1,7 @@
-import React from 'react';
 import { useWeatherStore } from '@/store/useWeatherStore';
-import ForecastItem from '../ForecastItem/ForecastItem';
-import { Stack } from 'react-bootstrap';
 import Link from 'next/link';
+import { Stack } from 'react-bootstrap';
+import ForecastItem from '../ForecastItem/ForecastItem';
 
 const ForecastList = () => {
   const { forecast } = useWeatherStore();
