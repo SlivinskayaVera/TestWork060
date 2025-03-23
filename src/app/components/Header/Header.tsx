8fe455c4b331388d6f4dad56d6b3dfce90f2f81f
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-03-23 15:19:04 +0300

    creating header component

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import { Navbar, Nav, Container } from 'react-bootstrap';
+
+const Header = () => {
+  return (
+    <header>
+      <Navbar bg='dark' data-bs-theme='dark'>
+        <Container>
+          <Navbar.Brand href='/'>Weather Forecast</Navbar.Brand>
+          <Nav className='me-auto'>
+            <Nav.Link href='/favorites'>Favorites</Nav.Link>
+            <Nav.Link href='/about'>About App</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </header>
+  );
+};
+
+export default Header;
