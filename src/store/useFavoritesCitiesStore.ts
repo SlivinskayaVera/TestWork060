a935d38ff3042efac039f914c7051d5b47c988c4
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-03-23 17:50:02 +0300

    changing state types in the favorite cities store

--- src/store/useFavoritesCitiesStore.ts
+++ src/store/useFavoritesCitiesStore.ts
@@ -1,19 +1,20 @@
+import { ICity } from '@/app/commonTypes/types';
 import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface IFavoriteCitiesStore {
-  favorites: string[];
-  addFavorite: (city: string) => void;
-  removeFavorite: (city: string) => void;
+  favorites: ICity[];
+  addFavorite: (city: ICity) => void;
+  removeFavorite: (city: ICity) => void;
 }
 
 export const useFavoritesCitiesStore = create<IFavoriteCitiesStore>()(
   persist(
     (set, get) => ({
       favorites: [],
-      addFavorite: (city: string) => set(() => ({ favorites: [...get().favorites, city] })),
-      removeFavorite: (city: string) =>
-        set(() => ({ favorites: [...get().favorites].filter((f: string) => f !== city) })),
+      addFavorite: (city) => set(() => ({ favorites: [...get().favorites, city] })),
+      removeFavorite: (city) =>
+        set(() => ({ favorites: [...get().favorites].filter((f) => f.name !== city.name) })),
     }),
     {
       name: 'favorites-storage',
