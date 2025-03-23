f6a75ac1eaeaec41714e708922f1ab5d21137efa
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-03-23 17:40:51 +0300

    creating fetchCurrentWeather and fetchWeatherForecast functions

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,5 +1,7 @@
-import axios from 'axios';
+import { ICity, IForecast } from '@/app/commonTypes/types';
+import axios, { isAxiosError } from 'axios';
 
+const BASE_URL = process.env.NEXT_PUBLIC_OPEN_WEATHER_MAP_URL;
 const GEO_URL = process.env.NEXT_PUBLIC_OPEN_WEATHER_MAP_GEO_URL;
 const API_KEY = process.env.NEXT_PUBLIC_OPEN_WEATHER_MAP_KEY;
 
@@ -13,4 +15,62 @@ export const convertCityNameToCityObject = async (cityName: string) => {
   }
 
   return response.data;
+};
+
+export const fetchCurrentWeather = async (city: ICity) => {
+  try {
+    const response = await axios(`${BASE_URL}/weather`, {
+      params: {
+        lat: city.lat,
+        lon: city.lon,
+        appid: API_KEY,
+        units: 'metric',
+      },
+    });
+
+    return response.data;
+  } catch (error) {
+    if (isAxiosError(error)) {
+      if (error.status === 400) {
+        throw 'Invalid request';
+      } else if (error.status === 401) {
+        throw 'Your request cannot be processed at the moment';
+      } else if (error.status === 404) {
+        throw 'Nothing was found for your query';
+      } else if (error.status === 429) {
+        throw 'Sorry, you have limited access';
+      }
+    } else {
+      throw 'An unexpected error has occurred, please try again later';
+    }
+  }
+};
+
+export const fetchWeatherForecast = async (city: ICity) => {
+  try {
+    const response = await axios(`${BASE_URL}/forecast`, {
+      params: {
+        lat: city.lat,
+        lon: city.lon,
+        appid: API_KEY,
+        units: 'metric',
+      },
+    });
+    
+    return response.data.list.filter((item: IForecast) => item.dt_txt.includes('12:00'));
+  } catch (error) {
+    if (isAxiosError(error)) {
+      if (error.status === 400) {
+        throw 'Invalid request';
+      } else if (error.status === 401) {
+        throw 'Your request cannot be processed at the moment';
+      } else if (error.status === 404) {
+        throw 'Nothing was found for your query';
+      } else if (error.status === 429) {
+        throw 'Sorry, you have limited access';
+      }
+    } else {
+      throw 'An unexpected error has occurred, please try again later';
+    }
+  }
 };
