da53d198d912f1d9e2d7f2d052afc2cd9246da41
Slivinskaya Vera <slivinskaya.vera@gmail.com>
2025-03-25 19:28:15 +0300

    importing ForecastItemWrapper component to home page and deleting unused imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,27 +1,17 @@
 'use client';
 
-import { useWeatherStore } from '@/store/useWeatherStore';
 import { Stack } from 'react-bootstrap';
 import SearchWithDropDown from './components/SearchWithDropDown/SearchWithDropDown';
-import Link from 'next/link';
-import ForecastItem from './components/ForecastItem/ForecastItem';
-import FavoriteCityButton from './components/FavoriteCityButton/FavoriteCityButton';
+import ForecastItemWrapper from './components/ForecastItemWrapper/ForecastItemWrapper';
 
 const HomePage = () => {
-  const { currentWeather, city } = useWeatherStore();
-
   return (
     <Stack gap={3} className='col-lg-7 mx-auto'>
       <h1>Weather</h1>
 
       <SearchWithDropDown />
-
-      {currentWeather && city && <ForecastItem weather={currentWeather} />}
-
-      <Stack gap={3} direction='horizontal'>
-        <FavoriteCityButton />
-        {city && <Link href={`/forecast/${city.name}`}>View the forecast for five days...</Link>}
-      </Stack>
+      
+      <ForecastItemWrapper />
     </Stack>
   );
 };
